271e8c6c8f79234677747d0a916efebfab750e15
kiki <jin991824@gmail.com>
2025-02-22 16:14:01 +0800

    Initial commit

---
+++ src/main/java/com/jill/stone_mall/StoneMallApplication.java
@@ -0,0 +1,13 @@
+package com.jill.stone_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jill/stone_mall/StoneMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jill.stone_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoneMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
